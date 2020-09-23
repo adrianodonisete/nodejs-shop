1c55a65d158ada1473e0c0a35247eb937513ecff
Adriano Machado <adrianodonisete@gmail.com>
2020-09-23 18:12:21 -0300

    create and get orders

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ class User {
     constructor(username, email, cart, id) {
         this.name = username;
         this.email = email;
-        this.cart = cart || { items: [] }; // {items: []}
+        this.cart = cart; // {items: []}
         this._id = id;
     }
 
@@ -63,6 +63,50 @@ class User {
                     };
                 });
             });
+    }
+
+    deleteItemFromCart(productId) {
+        const updatedCartItems = this.cart.items.filter(item => {
+            return item.productId.toString() !== productId.toString();
+        });
+        const db = getDb();
+        return db
+            .collection("users")
+            .updateOne(
+                { _id: new ObjectId(this._id) },
+                { $set: { cart: { items: updatedCartItems } } }
+            );
+    }
+
+    addOrder() {
+        const db = getDb();
+        return this.getCart()
+            .then(products => {
+                const order = {
+                    items: products,
+                    user: {
+                        _id: new ObjectId(this._id),
+                        name: this.name,
+                    },
+                };
+                return db.collection("orders").insertOne(order);
+            })
+            .then(result => {
+                this.cart = { items: [] };
+                return db
+                    .collection("users")
+                    .updateOne(
+                        { _id: new ObjectId(this._id) },
+                        { $set: { cart: { items: [] } } }
+                    );
+            });
+    }
+
+    getOrders() {
+        const db = getDb();
+        return db.collection('orders') 
+            .find({'user._id': new ObjectId(this._id)})
+            .toArray();
     }
 
     static findById(userId) {
