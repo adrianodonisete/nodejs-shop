aa619ea479f6f51ea01144d9f541caab2890e011
Adriano Machado <adrianodonisete@gmail.com>
2020-09-23 19:49:43 -0300

    add mongoose on project

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const db = require('./util/dbConfig');
 
 const errorController = require('./controllers/errorController');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -18,27 +19,30 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    User.findById("5f6a7c163a89fcd5408bbf0e")
-        .then(user => {
-            if (!user) {
-                //req.user = new User('adriano', 'adriano@teste', {items: []}, "5baa2528563f16379fc8a610");
-            } else {
-                req.user = new User(user.name, user.email, user.cart, user._id);
-            }
-            next();
-        })
-        .catch(err => console.log(err)); 
-});
+// app.use((req, res, next) => {
+//     User.findById("5f6a7c163a89fcd5408bbf0e")
+//         .then(user => {
+//             if (!user) {
+//                 //req.user = new User('adriano', 'adriano@teste', {items: []}, "5baa2528563f16379fc8a610");
+//             } else {
+//                 req.user = new User(user.name, user.email, user.cart, user._id);
+//             }
+//             next();
+//         })
+//         .catch(err => console.log(err)); 
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-    app.listen(3000, 
-        () => console.log(`Listen at http://localhost:3000/`));
-});
-
-
+mongoose
+    .connect(db.config, { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+    })
+    .then(rs =>
+        app.listen(3000, () => console.log(`Listen at http://localhost:3000/`))
+    )
+    .catch(err => console.log(err));
---
+++ util/dbConfig.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_HOST = process.env.DB_HOST;
+
+exports.config = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true`;
+
+/**
+ * 
+ * `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true`,
+        { useUnifiedTopology: true } 
+*/
